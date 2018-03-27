ecbb07fe7d79df0aa215a78bcb26935c891f7314
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 21:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name){
+funion takeANumber(katzDeliLine, name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 
